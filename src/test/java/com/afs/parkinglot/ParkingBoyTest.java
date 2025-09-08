c20e33f35a55d017aad55c77d5446ef76bc51bae
liule13 <LEO.LIU@OOCL.COM>
2025-09-08 20:45:18 +0800

    test: add should_return_null_when_park_given_full_parking_lot

--- src/test/java/com/afs/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/afs/parkinglot/ParkingBoyTest.java
@@ -1,10 +1,22 @@
 package com.afs.parkinglot;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingBoyTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    // AC: Given a parking ticket and the parking lot has available positions, when parking, then returned after parking a car.
     @Test
     public void should_return_ticket_when_park_given_available_position() {
         ParkingLot parkingLot = new ParkingLot();
@@ -16,6 +28,8 @@ public class ParkingBoyTest {
 
         assertEquals(ticket, expectedTicket);
     }
+
+    // AC: Given a parking ticket and the parking lot has a parked car, when fetching, then return the car.
     @Test
     public void should_return_car_when_fetch_given_valid_ticket() {
         ParkingLot parkingLot = new ParkingLot();
@@ -27,4 +41,20 @@ public class ParkingBoyTest {
 
         assertEquals(car, fetchedCar);
     }
+
+    // AC: Given a parking ticket and the parking lot is full, when parking, then return null and print "No available position.".
+    @Test
+    public void should_return_null_when_park_given_full_parking_lot() {
+        ParkingLot parkingLot = new ParkingLot(1);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car1 = new Car("粤A12345");
+        Car car2 = new Car("粤B12345");
+        parkingBoy.park(car1);
+
+        ParkingTicket ticket2 = parkingBoy.park(car2);
+
+        assertNull(ticket2);
+        assertTrue(outContent.toString().contains("No available position."));
+    }
+
 }
